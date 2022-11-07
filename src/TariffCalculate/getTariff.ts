5ce7d8725da4532d541c8cca7a35acc2f70ef1ac
Sergei Murashow <sergey.murashow@gmail.com>
2022-11-07 14:02:29 +1000

    Disable getTariff

--- src/TariffCalculate/getTariff.ts
+++ src/TariffCalculate/getTariff.ts
@@ -1,85 +1,85 @@
-import { LoDashStatic } from "lodash"
-import { Obj } from "src/types/types"
+// import { LoDashStatic } from "lodash"
+// import { Obj } from "src/types/types"
 
-// const moment = require('moment')
-const _: LoDashStatic = require('lodash')
-
-
-import { VoyageData } from "./VoyageData"
-
-import { ContainersParse } from "./ContainerData"
-import { Tariff } from "./Tariff"
-import { sendTariff } from "../callbacks/callbackTariff"
-import { Discount } from "./Discount"
-
-// import { data } from "./testData/data"
-// import { contractData } from "./testData/contractData";
-// import { tariffList as tariffInput } from "./testData/tariffList"
-// const voyageData = data
-// const tariff = contractData.values[11]
-// const containersData = voyageData[144]
-
-// let TEST = new Tariff()
-// TEST.tariffList = tariffInput
-
-// let TEST = getTariff({ voyageData, tariffInput, contractData })
-// let t
+// // const moment = require('moment')
+// const _: LoDashStatic = require('lodash')
 
 
-export function getTariff({ voyageData, tariffInput, contractData }) {
+// import { VoyageData } from "./VoyageData"
 
-	let voyageFields = voyageData.values
-	let voyageParsed = new VoyageData(
-		voyageFields[165],
-		{
-			163: voyageFields[163],
-			164: voyageFields[164]
-		}
-	).data
+// import { ContainersParse } from "./ContainerData"
+// import { Tariff } from "./Tariff"
+// import { sendTariff } from "../callbacks/callbackTariff"
+// import { Discount } from "./Discount"
 
-	let discount = {
-		find({ port, direction, socCoc, type }) {
-			return { discount: 0 }
-		}
-	}
-	let contractDiscounts
+// // import { data } from "./testData/data"
+// // import { contractData } from "./testData/contractData";
+// // import { tariffList as tariffInput } from "./testData/tariffList"
+// // const voyageData = data
+// // const tariff = contractData.values[11]
+// // const containersData = voyageData[144]
 
-	if( contractData ){
-		contractDiscounts = contractData.values[11]
-		discount = new Discount(contractDiscounts)
-	}
+// // let TEST = new Tariff()
+// // TEST.tariffList = tariffInput
+
+// // let TEST = getTariff({ voyageData, tariffInput, contractData })
+// // let t
+
+
+// export function getTariff({ voyageData, tariffInput, contractData }) {
+
+// 	let voyageFields = voyageData.values
+// 	let voyageParsed = new VoyageData(
+// 		voyageFields[165],
+// 		{
+// 			163: voyageFields[163],
+// 			164: voyageFields[164]
+// 		}
+// 	).data
+
+// 	let discount = {
+// 		find({ port, direction, socCoc, type }) {
+// 			return { discount: 0 }
+// 		}
+// 	}
+// 	let contractDiscounts
+
+// 	if( contractData ){
+// 		contractDiscounts = contractData.values[11]
+// 		discount = new Discount(contractDiscounts)
+// 	}
 	
-	let tariff = new Tariff()
-	tariff.tariffList = tariffInput
+// 	let tariff = new Tariff()
+// 	tariff.tariffList = tariffInput
 
-	let containersParsed = new ContainersParse(voyageParsed, voyageFields['144'])
-		.parsed
+// 	let containersParsed = new ContainersParse(voyageParsed, voyageFields['144'])
+// 		.parsed
 
-	let resp: Obj = {}
-	resp.containers = containersParsed.map(m => {
-		let tariffForContainer = tariff.find(m)
-		if (!tariffForContainer) throw new Error(`I don't find tariff :(`)
-		let containerDiscount = discount.find(m)
+// 	let resp: Obj = {}
+// 	resp.containers = containersParsed.map(m => {
+// 		let tariffForContainer = tariff.find(m)
+// 		if (!tariffForContainer) throw new Error(`I don't find tariff :(`)
+// 		let containerDiscount = discount.find(m)
 
-		let ans = {
-			price: tariffForContainer.price,
-			discount: containerDiscount.discount,
-			cost: tariffForContainer.price - containerDiscount.discount
-		}
+// 		let ans = {
+// 			price: tariffForContainer.price,
+// 			discount: containerDiscount.discount,
+// 			cost: tariffForContainer.price - containerDiscount.discount
+// 		}
 
-		return Object.assign({}, m, ans)
-	})
+// 		return Object.assign({}, m, ans)
+// 	})
 
-	resp.voyage = {
-		catalogId: voyageData.catalogId,
-		recordId: voyageData.id
-	}
+// 	resp.voyage = {
+// 		catalogId: voyageData.catalogId,
+// 		recordId: voyageData.id
+// 	}
 
-	console.log(JSON.stringify(resp, null, 1))
+// 	console.log(JSON.stringify(resp, null, 1))
 
-	sendTariff(resp)
+// 	sendTariff(resp)
 
-}
+// }
 
 
 
--- src/tariffProcess.ts
+++ src/tariffProcess.ts
@@ -1,16 +1,16 @@
-import { downloadFiles } from './utils/downloadFiles'
-import { createCatalogs } from './utils/createCatalogs'
-import { mergeByBookingId } from './mergeByBookingId'
-import { sendParsed } from './callbacks/callbackParsedDocs'
-import { Obj } from './types/types'
-import Path from 'path'
-import Fs from 'fs'
+// import { downloadFiles } from './utils/downloadFiles'
+// import { createCatalogs } from './utils/createCatalogs'
+// import { mergeByBookingId } from './mergeByBookingId'
+// import { sendParsed } from './callbacks/callbackParsedDocs'
+// import { Obj } from './types/types'
+// import Path from 'path'
+// import Fs from 'fs'
 
-import { getTariff } from './TariffCalculate/getTariff'
+// import { getTariff } from './TariffCalculate/getTariff'
 
 
-async function tariffProcess( {voyageData, tariffInput, contractData} ) {
-	await getTariff( {voyageData, tariffInput, contractData} )
-}
+// async function tariffProcess( {voyageData, tariffInput, contractData} ) {
+// 	await getTariff( {voyageData, tariffInput, contractData} )
+// }
 
-module.exports = tariffProcess
+// module.exports = tariffProcess
