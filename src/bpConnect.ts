ff9b3448c05019415b343fdf60cd23fb0eaa9b93
Sergei Murashow <sergey.murashow@gmail.com>
2022-12-17 00:31:54 +1000

    comment

--- src/bpConnect.ts
+++ src/bpConnect.ts
@@ -1,6 +1,6 @@
 const BP = require('bp-api');
 const connection = require('../../config/default.json').connection;
-console.log( 'bpium connect')
+console.log( 'Trying to connect to Bpium')
 const bpiumConnect  = new BP(connection.domen, connection.username, connection.password, connection.protocol, 30000);
 console.log( 'Bpium is connected', bpiumConnect)
 
